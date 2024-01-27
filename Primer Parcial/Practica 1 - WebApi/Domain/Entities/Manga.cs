2f1f256cc1246842242e259e2d8e93c66c076d72
Jacobo <jakobgmr2@gmail.com>
2024-01-27 12:36:41 -0600

    files removed

--- Primer Parcial/Practica 1 - WebApi/Domain/Entities/Manga.cs
+++
@@ -1,9 +0,0 @@
-namespace JaveragesLibrary.Domain.Entities;
-
-public class Manga
-{
-    public int Id { get; set; }
-    public required string Title { get; set; }
-    public required string Author { get; set; }
-    public DateTime PublicationDate { get; set; }
-}
--- Primer Parcial/Practica 4 - My First Use Of Nuget Packages/Domain/Dtos/MangaCreateDTO.cs
+++
@@ -1,7 +0,0 @@
-namespace JaveragesLibrary.Domain.Dtos;
-
-public class MangaCreateDTO
-{
-    public string Title { get; set; } = null!;
-    public string Author { get; set; } = null!;
-}
